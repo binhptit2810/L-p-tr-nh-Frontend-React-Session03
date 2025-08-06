643c2cd8ad619649c052be91931e11b9e80f3d97
Your Name <you@example.com>
2025-08-06 21:29:58 +0700

    nop bai tap ss03

--- Session03/Bai01.ts
+++ Session03/Bai01.ts
@@ -1,23 +1,9 @@
-type NguoiDung = {
-    id: number;
-    ten: string;
-    tuoi: number;
-    diachi: string;
-};
-let danhSachNguoiDung: NguoiDung[] = [
-    {
-        id: 1,
-        ten: "Nguyen Van A",
-        tuoi: 20,
-        diachi: "Ha Noi"
-    },
-    {
-        id: 2,
-        ten: "Tran Thi B",
-        tuoi: 22,
-        diachi: "Da Nang"
-    }
-];
-danhSachNguoiDung.forEach(nguoi => {
-    console.log(`ID: ${nguoi.id}, Ten: ${nguoi.ten}, Tuoi: ${nguoi.tuoi}, Dia chi: ${nguoi.diachi}`);
-});
+let username = "binh";
+let age = 17;
+let job = "hoc sinh";
+function hienThongTin(name, age, job) {
+    console.log("Ten:", name);
+    console.log("Tuoi:", age);
+    console.log("Nghe nghiep:", job);
+}
+hienThongTin(username, age, job);
